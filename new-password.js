87dc3c5043ef412e2eb79dcfb3f93517ad4a1775
Guilherme Nass <nassguilherme@gmail.com>
2018-10-29 00:20:02 -0300

    implementação de envio de email ao realizar cadastro de usuario

--- new-password.js
+++ new-password.js
@@ -3,10 +3,8 @@ function saveNewPassword(event){
     event.preventDefault();
     let params = new URLSearchParams(document.location.search.substring(1));
     let userId = Number(params.get("id"));
-    let token = params.get("token");
 
     localStorage.clear();
-    localStorage.setItem('token', token);
 
     if($(".form-new-password").val() != $(".form-new-password-confirm").val()){
         swal("", "As senhas não conferem!", "error");
@@ -30,12 +28,9 @@ function saveNewPassword(event){
             if(data.status === 401)
                 swal(
                     "",
-                    "O tempo para recuperar a senha, expirou. Tente realizar o procedimento novamente!",
+                    "Ocorreu um erro ao tentar salvar a nova senha!",
                     "error"
                 );
-        },
-        beforeSend: function(xhr, settings) {
-            xhr.setRequestHeader('Authorization','Bearer ' + token );
         }
     })
 }
